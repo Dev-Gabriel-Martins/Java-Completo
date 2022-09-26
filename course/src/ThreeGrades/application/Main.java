e70707e44803146264dc79ba1a586b6ce7f024d6
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-26 10:46:43 -0300

    add exercício ThreeGrades

---
+++ course/src/ThreeGrades/application/Main.java
@@ -0,0 +1,32 @@
+
+package ThreeGrades.application;
+
+import ThreeGrades.entities.Student;
+import java.util.Locale;
+import java.util.Scanner;
+/**
+ *
+ * @author gabs
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Student student = new Student();
+        
+        System.out.println("Enter student name: ");
+        student.name = sc.nextLine();
+        
+        System.out.println("Enter grades for the three semesters:");
+        student.firstTrimester = sc.nextDouble();
+        student.secundTrimester = sc.nextDouble();
+        student.thirdTrimester = sc.nextDouble();
+        
+        System.out.print(student);
+        student.verificationGrade();
+        
+        
+    }               
+            
+}
---
+++ course/src/ThreeGrades/entities/Student.java
@@ -0,0 +1,33 @@
+
+
+package ThreeGrades.entities;
+
+/**
+ *
+ * @author gabs
+ */
+public class Student {
+    
+    public String name;
+    public double firstTrimester;
+    public double secundTrimester;
+    public double thirdTrimester;
+    
+    public double finalGrade(){
+    return firstTrimester + secundTrimester + thirdTrimester;
+    }
+    
+    public String toString(){
+    return "Final grade = "+ String.format("%.2f%n", finalGrade());
+    }
+    
+    public void verificationGrade(){
+        if(finalGrade() > 60){            
+            System.out.println("Pass");
+        }else{
+            System.out.println("Failed");
+            double fg = 60 - finalGrade();
+            System.out.println("Missing" + String.format("%.2f",fg) +" points");
+        }
+    }
+}
