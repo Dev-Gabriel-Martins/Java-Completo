33e19dee7c5bfd357003955948e5bf3a99a9c679
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-26 17:17:00 -0300

    corrigido exercício  CurrencyConverter

--- course/src/CurrencyConverter/util/CurrencyConverter.java
+++ course/src/CurrencyConverter/util/CurrencyConverter.java
@@ -10,11 +10,13 @@ public class CurrencyConverter {
     public static double calc (double quatation, double quantity){
     double dollar = quatation * quantity;
     
-    double IOF = (dollar * 0.6) / 100;        
+    double IOF = (dollar * 6) / 100;        
         System.out.println("IOF: "+ IOF);
         
-        return dollar - IOF;
+        return dollar + IOF;
     }
+    
+    
 }
 
 
