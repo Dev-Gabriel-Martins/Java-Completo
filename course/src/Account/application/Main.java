25c6a41cc31f42d690ee6982702465e6b2419077
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-28 14:49:21 -0300

    corrigido exercício Account

--- course/src/Account/application/Main.java
+++ course/src/Account/application/Main.java
@@ -12,25 +12,29 @@ public class Main {
    public static void main(String[] args){
        Locale.setDefault(Locale.US);
        Scanner sc = new Scanner(System.in);       
-       Account acc = new Account();
+       Account acc;
         
        System.out.print("Entrer accout number: ");
        int id = sc.nextInt();
-       acc.newId(id);
-       
+
        sc.nextLine();
        System.out.print("Enter accout holder: ");
        String name = sc.nextLine();
-       acc.setName(name);
+     
+  
        
        System.out.print("is there na initial deposit value (y/n) ");
        char answer = sc.next().charAt(0);
        
        if(answer == 'y'){
            System.out.print("Enter initial deposit value: ");
-           double deposit = sc.nextDouble();
-           acc.deposit(deposit);
+           double initDeposit = sc.nextDouble();
+          acc = new Account(id, name,initDeposit);
+       }else{
+           acc = new Account(id, name);
        }
+       
+       
        
        System.out.println();
        System.out.println("Accout data:");
--- course/src/Account/entities/Account.java
+++ course/src/Account/entities/Account.java
@@ -11,14 +11,22 @@ public class Account {
     private String name;
     private double account;    
     
+    public Account(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    
+    public Account(int id, String name, double initDeposit){
+        this.id = id;
+        this.name = name;
+        deposit(initDeposit);
+    }
+    
     //get e set
     public int getId(){
         return id;
     }
     
-    public void newId(int id){
-        this.id = id;
-    }
     
     public String getName(){
         return name;
@@ -34,11 +42,11 @@ public class Account {
     }
     
     public void withdraw(double withdraw){
-        account = account - withdraw - 5.00;
+        account -= withdraw + 5.00;
     }
     
     public String toString(){
-        return"Account "+id+", holder: "+name+", Balance: "+String.format("%.2f%n", account);
+        return"Account "+id+", holder: "+name+", Balance $: "+String.format("%.2f%n", account);
     }
     
 
