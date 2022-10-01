bf8d709b83e9c88d814205f187df146960946dea
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-09-30 22:13:39 -0300

    add SalaryIncrease

---
+++ course/src/SalaryIncrease/application/Main.java
@@ -0,0 +1,21 @@
+
+package SalaryIncrease.application;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+  public static void main(String[] args){
+      
+      Locale.setDefault(Locale.US);
+      Scanner sc = new Scanner(System.in);
+      
+      System.out.println("How many employees will be registered? ");
+      int n = sc.nextInt();
+      
+      
+      List<String> cc = new ArrayList<>();
+      
+  }  
+}
---
+++ course/src/SalaryIncrease/entities/Payslip.java
@@ -0,0 +1,26 @@
+
+package SalaryIncrease.entities;
+
+
+public class Payslip {
+    
+    private String name;
+    private Double salary;
+    
+    public Payslip(){
+    
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+}
