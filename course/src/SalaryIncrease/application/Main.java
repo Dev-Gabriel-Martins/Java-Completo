c644ddfb50c77b982499ddac5b89ccac3def2ff1
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2022-10-12 22:07:10 -0300

    correção Payslip.java

--- course/src/SalaryIncrease/application/Main.java
+++ course/src/SalaryIncrease/application/Main.java
@@ -1,5 +1,6 @@
 
 package SalaryIncrease.application;
+import SalaryIncrease.entities.Payslip;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -15,7 +16,63 @@ public class Main {
       int n = sc.nextInt();
       
       
-      List<String> cc = new ArrayList<>();
+      List<Payslip> list = new ArrayList<>();
       
-  }  
-}
+      
+      for(int i =0; i < n;i++){
+      System.out.println();
+      System.out.println("Emplyooee #"+ (i+1) +":");
+      System.out.print("Id:");
+      Integer id = sc.nextInt();
+      
+      
+      System.out.print("Name: ");
+      sc.nextLine();
+      String name = sc.nextLine(); 
+      
+      System.out.print("Salary: ");
+      Double salary = sc.nextDouble();
+      
+      Payslip ps = new Payslip(id, name, salary);
+      
+      list.add(ps);
+      }
+      
+      System.out.print("Enter the employee id that will have salary increase:");
+      Integer idIncrease = sc.nextInt();
+      Integer pos = position(list, idIncrease);
+      if (pos == null){
+          
+      System.out.println("Does not exist this id number!");
+      
+      }else{
+      
+      System.out.println("Enter the percentage: ");    
+      Double percentage = sc.nextDouble();
+      list.get(pos).increseSalary(percentage);
+      }
+      
+      System.out.println();
+      System.out.println("List of employess: ");
+      for(Payslip ps : list){
+      
+      System.out.println(ps);
+      
+      }
+      
+      
+      sc.close();
+  }
+  
+  public static Integer position(List<Payslip> list, int id){      
+    
+      for(int i =0 ; i < list.size(); i++){
+         if(list.get(i).getId() == id){
+         return i;
+         } 
+      
+        }
+      return null;
+    }
+  
+} 
--- course/src/SalaryIncrease/entities/Payslip.java
+++ course/src/SalaryIncrease/entities/Payslip.java
@@ -4,11 +4,23 @@ package SalaryIncrease.entities;
 
 public class Payslip {
     
+    private Integer id;
     private String name;
     private Double salary;
     
     public Payslip(){
     
+    }
+
+    public Payslip(Integer id, String name, Double salary){
+    super();
+    this.id = id;
+    this.name = name;
+    this.salary = salary;          
+    }
+
+    public Integer getId() {
+        return id;
     }
 
     public String getName() {
@@ -18,9 +30,20 @@ public class Payslip {
     public Double getSalary() {
         return salary;
     }
-    
-    public void setName(String name){
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
     
+    public void increseSalary(Double percentage){
+       this.salary = salary + salary * percentage /100;
+    
+    }
+    public String toString(){
+       return id + " , " + name +" , "+ String.format("%.2f", salary) ;
+    }
 }
