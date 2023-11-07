1ce3a6c5880396760ab31455cd2754a489697a47
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2023-11-07 06:54:37 -0300

    Studying Inheritance Override

---
+++ Inheritance/Account - Override/src/application/Program.java
@@ -0,0 +1,28 @@
+
+package application;
+
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
+/**
+ *
+ * @author gabri
+ */
+public class Program {
+    public static void main(String[] args){
+        
+        Account acc1 = new Account(1001, "Alex", 1000.0);
+        acc1.withdraw(200.0);
+        System.out.println(acc1.getBalance());
+        
+        Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+        acc2.withdraw(200.0);
+        System.out.println(acc2.getBalance());
+                
+                
+        
+
+        
+    }
+}
