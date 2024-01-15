b9d47d5296ba1aa8e9e32e8196d99d48081f95a1
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2024-01-15 09:01:30 -0300

    update main

--- Inheritance/TaxPayser/src/application/Program.java
+++ Inheritance/TaxPayser/src/application/Program.java
@@ -1,9 +1,22 @@
 
 package application;
-
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+      
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the number of tax payers");
+        int i = sc.nextInt();
+        
+        for(int j=0;j>i;j++){
+            System.out.println("Tax payer #"+(i+1)+" data:");
+        
+        
+        }
+        
+        
+        
+     }
 }
