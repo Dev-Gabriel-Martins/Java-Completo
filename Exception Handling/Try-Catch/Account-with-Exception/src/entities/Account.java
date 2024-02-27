0383145964356c592fb71add7e686ba1dce15288
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2024-02-27 06:28:49 -0300

    created class account structure II

--- Exception Handling/Try-Catch/Account-with-Exception/src/entities/Account.java
+++ Exception Handling/Try-Catch/Account-with-Exception/src/entities/Account.java
@@ -5,5 +5,32 @@ public class Account {
     private String holder;
     private Double balance;
     private Double withdrawLimit;
+
+
+    public Account(){}
+    
+    public Account(Integer number, String holder, Double balance, Double withdrawLimit){
+     this.number = number;
+     this.holder = holder;
+     this.balance = balance;
+     this.withdrawLimit = withdrawLimit;        
+    }
+    
+    public void deposit(Double amount){
+    
+    }
+    
+    public void withdraw(Double amount){
+    
+    }
+
+
+
+
+
+
+
+
+
 }
     
