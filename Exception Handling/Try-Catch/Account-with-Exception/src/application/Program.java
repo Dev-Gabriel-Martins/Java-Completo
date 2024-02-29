13f7d04c5878bbe8066256982b0017e8df475c55
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2024-02-29 07:13:35 -0300

    update Account-with-Exception

--- Exception Handling/Try-Catch/Account-with-Exception/src/application/Program.java
+++ Exception Handling/Try-Catch/Account-with-Exception/src/application/Program.java
@@ -1,5 +1,7 @@
 
 package application;
+import entities.Account;
+import exceptions.BusinessException;
 import java.util.Scanner;
 /**
  *
@@ -11,7 +13,30 @@ public class Program {
         Scanner sc = new Scanner(System.in);
         
         System.out.println("Enter account data");
-        System.out.println("Number");
-    
+        System.out.println("Number:");
+        Integer number =  sc.nextInt();
+        System.out.println("Holder: ");
+        String holder = sc.nextLine();
+        System.out.println("Initial balance:");
+        Double balance = sc.nextDouble();
+        System.out.println("withdraw limit:");
+        Double withdrawLimit = sc.nextDouble();
+        
+        Account acc = new Account(number, holder, balance, withdrawLimit);
+        
+        System.out.println("Enter amount for withdraw:");
+        double amount = sc.nextDouble();
+        
+        
+		try {
+			acc.withdraw(amount);
+			System.out.println("New balance: %.2f%n", acc.getBalance());
+		}
+		catch (BusinessException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		sc.close();
+       
     }
 }
--- Exception Handling/Try-Catch/Account-with-Exception/src/entities/Account.java
+++ Exception Handling/Try-Catch/Account-with-Exception/src/entities/Account.java
@@ -1,5 +1,7 @@
 package entities;
 
+import exceptions.BusinessException;
+
 public class Account {
     private Integer number;
     private String holder;
@@ -16,20 +18,55 @@ public class Account {
      this.withdrawLimit = withdrawLimit;        
     }
     
-    public void deposit(Double amount){
-    
-    }
-    
-    public void withdraw(Double amount){
-    
-    }
+   public Integer getNumber() {
+		return number;
+	}
 
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
 
+	public String getHolder() {
+		return holder;
+	}
 
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
 
+	public Double getBalance() {
+		return balance;
+	}
 
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
 
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
 
+	public void setWithdrawLimit(Double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(double amount) {
+		validateWithdraw(amount);
+		balance -= amount;
+	}
+	
+	private void validateWithdraw(double amount) {
+		if (amount > getWithdrawLimit()) {
+			throw new BusinessException("Erro de saque: A quantia excede o limite de saque");
+		} 
+		if (amount > getBalance()) {
+			throw new BusinessException("Erro de saque: Saldo insuficiente");
+		}
+	}
 
 
 }
