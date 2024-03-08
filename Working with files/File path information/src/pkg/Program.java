130fbfc124ba9cb2d37ba508126b71bfdde13a3a
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2024-03-08 12:42:49 -0300

    add File path information

---
+++ Working with files/File path information/src/pkg/Program.java
@@ -0,0 +1,24 @@
+
+package pkg;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter a folder path: ");
+        String strPath = sc.nextLine();
+        
+        File path = new File(strPath);
+        
+        System.out.println("getPath: "+ path.getPath());
+        System.out.println("getParent: "+ path.getParent());
+        System.out.println("getName: "+ path.getName());    
+        
+        sc.close();
+        
+    }
+}
