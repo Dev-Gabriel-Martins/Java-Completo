646d1abaf29faa9963b4649e9e0a2c9d3d2c09aa
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2024-03-09 13:56:26 -0300

    Update Program.java

--- Working with files/Exercise Files/src/pkg/Program.java
+++ Working with files/Exercise Files/src/pkg/Program.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -15,14 +16,16 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        String path = "g:\\input.csv";        
+        String path = "g:\\input.csv";
+        List<Product> product = new ArryList<product>();
         
         try(BufferedReader br = new BufferedReader(new FileReader(path))){            
             String line = br.readLine();
             String[] values = line.split(",");
         for (String value : values) {
             System.out.println(value);
-        } //TODO Passar o value para dentro dos objeto  Product            
+        } //TODO Passar o value para dentro dos objeto  Product
+            // provalemente com List
         }catch(IOException e){
             System.out.println("Error "+ e.getMessage());
         }
