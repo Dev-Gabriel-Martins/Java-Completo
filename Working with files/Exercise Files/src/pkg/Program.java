f435c0d8c8c31a1728ce880209e26054798764c2
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2024-03-10 22:47:15 -0300

    Update Program.java

--- Working with files/Exercise Files/src/pkg/Program.java
+++ Working with files/Exercise Files/src/pkg/Program.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
@@ -17,7 +18,7 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         String path = "g:\\input.csv";
-        List<Product> product = new ArryList<product>();
+        List<Product> product = new ArrayList<Product>();
         
         try(BufferedReader br = new BufferedReader(new FileReader(path))){            
             String line = br.readLine();
