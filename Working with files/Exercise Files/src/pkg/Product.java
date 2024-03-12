106c47029ed556f7f123da1e082375254c894f83
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2024-03-12 20:58:03 -0300

    Corrected Exercise Files

--- Working with files/Exercise Files/src/pkg/Product.java
+++ Working with files/Exercise Files/src/pkg/Product.java
@@ -20,8 +20,33 @@ public class Product {
         this.price = price;
         this.quantity = quantity;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
     
-    private double totalPrice(){
+    
+    public double totalPrice(){
     return price * quantity;
     }
     
--- Working with files/Exercise Files/src/pkg/Program.java
+++ Working with files/Exercise Files/src/pkg/Program.java
@@ -1,8 +1,10 @@
-
 package pkg;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -12,32 +14,54 @@ import java.util.Scanner;
 
 public class Program {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws ParseException {
+
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
         String path = "g:\\input.csv";
-        List<Product> product = new ArrayList<Product>();
-        
-        try(BufferedReader br = new BufferedReader(new FileReader(path))){            
-            String line = br.readLine();
-            String[] values = line.split(",");
-            
-            System.out.println(values[1]);
-            
-            
-            
-            /*for (String value : values) {
-            System.out.println(value);
-        } */
-        }catch(IOException e){
-            System.out.println("Error "+ e.getMessage());
+        List<Product> list = new ArrayList<Product>();
+
+        File sourceFile = new File(path);
+
+        //Create folder \out
+        boolean sucess = new File(sourceFile.getParent() + "\\out").mkdir();
+
+        //Create file in folder \out
+        String targetFileStr = sourceFile.getParent() + "\\out\\summary.csv";
+
+        // Cada vezes que while gira, ele le um nova linha e
+        //Cada linha do .readline vai se tornar um String List,
+        // a onde eu vou separar os valores pelo .split
+        // e carregar pela posicao no objeto
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String itemCsv = br.readLine();
+            while (itemCsv != null) {
+                String[] fields = itemCsv.split(",");
+                String name = fields[0];
+                double price = Double.parseDouble(fields[1]);
+                int quantity = Integer.parseInt(fields[2]);
+
+                list.add(new Product(name, price, quantity));
+
+                itemCsv = br.readLine();
+            }
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+                for(Product item: list){
+                    bw.write(item.getName() + "," + item.totalPrice());
+                    bw.newLine();
+                }
+            } catch (IOException e) {
+                System.out.println("Erro: " + e.getMessage());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Erro: " + e.getMessage());
         }
-        
-        
-        
+
         sc.close();
     }
-    
+
 }
